ac5f2f8373fb6dffc0d91004522b36ba0d23a962
seidai-suzuki <vivienne0801.ss@gmail.com>
2020-09-25 23:19:42 +0900

    add : マイグレーションファイル追加

---
+++ database/migrations/2020_09_25_140325_create_shops_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('address');
+            $table->tinyinteger('rate')->default(3);
+            $table->tinyinteger('type')->default(0);
+            $table->longText('memo')->nullable();
+            $table->tinyinteger('favorite_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
